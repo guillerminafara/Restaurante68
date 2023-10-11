a192d41024fc8fab7e3e7589becf1b42e2e56387
Jonajgp <jagp@live.com.ar>
2023-10-11 13:08:04 -0300

    completado pedidosData 11/10
    ya estan todos los listar

--- Resto68/src/AcessoADatos/PedidoData.java
+++ Resto68/src/AcessoADatos/PedidoData.java
@@ -8,8 +8,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import javax.swing.JOptionPane;
 
@@ -22,7 +25,7 @@ public class PedidoData {
     }
     
     public void agregarPedido(Pedido pedido){
-        String sql ="INSERT INTO pedido (idMesa, nombreMesero, fechaHora, importe, cobrada) VALUES(?,?,?,?,?)";
+        String sql ="INSERT INTO pedido (idMesa, mesero, fechaHora, importe, cobrada) VALUES(?,?,?,?,?)";
         java.sql.Timestamp sqlTimestamp = java.sql.Timestamp.valueOf(pedido.getFechaHora());
         try {
             PreparedStatement ps= con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS );
@@ -38,7 +41,7 @@ public class PedidoData {
                 if(pedido.getIdMesa()==pedidoDeLista.getIdMesa()&&!(pedido.getNombreMesero().equalsIgnoreCase(pedidoDeLista.getNombreMesero())))
                         bandera=false;
             }
-            if (!bandera) {
+            if (bandera) {
                 ps.executeUpdate();
                 ResultSet rs = ps.getGeneratedKeys();
                 if (rs.next()) {
@@ -73,7 +76,7 @@ public class PedidoData {
     }
     
     public void modificarPedido(Pedido pedido) {
-        String sql = "UPDATE pedido SET idMesa=?, nombreMesero=?, fechaHora=?, importe=?, cobrada=? WHERE idPedido=? ";
+        String sql = "UPDATE pedido SET idMesa=?, mesero=?, fechaHora=?, importe=?, cobrada=? WHERE idPedido=? ";
         java.sql.Timestamp sqlTimestamp = java.sql.Timestamp.valueOf(pedido.getFechaHora());
         try {
             PreparedStatement ps = con.prepareStatement(sql);
@@ -82,16 +85,26 @@ public class PedidoData {
             ps.setTimestamp(3, sqlTimestamp);
             ps.setDouble(4, pedido.getImporte());
             ps.setBoolean(5, pedido.isCobrada());
-
-            int exito = ps.executeUpdate();
-            if (exito == 1) {
-                JOptionPane.showMessageDialog(null, "Producto modificado");
+            List<Pedido> listaDePedidos = new ArrayList<>();
+            listaDePedidos = listarPedidos();
+            boolean bandera = true;
+            for(Pedido pedidoDeLista:listaDePedidos){
+                if(pedido.getIdMesa()==pedidoDeLista.getIdMesa()&&!(pedido.getNombreMesero().equalsIgnoreCase(pedidoDeLista.getNombreMesero())))
+                        bandera=false;
             }
-
+            if(bandera){
+                int exito = ps.executeUpdate();
+                if (exito == 1)
+                    JOptionPane.showMessageDialog(null, "Producto modificado");
+            }else {
+                JOptionPane.showMessageDialog(null, "La mesa corresponde a otro mesero");
+            }
+            ps.close();
         } catch (SQLException ex) {            
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla producto" + ex.getMessage());
 
         }
+        
     }
     
     public List<Pedido> listarPedidos(){
@@ -119,5 +132,80 @@ public class PedidoData {
         
         return listaDePedidos;
     }
+    public List<Pedido> listarPedidosPorMesa(int idMesa){
+        List<Pedido> pedidosPorMesa = new ArrayList<>();
+        for(Pedido pedido :this.listarPedidos()){
+            if(idMesa==pedido.getIdMesa())
+                pedidosPorMesa.add(pedido);
+        }
+        Collections.sort(pedidosPorMesa,comparaMesa);
+        return pedidosPorMesa;
+    }
     
+    public static Comparator<Pedido> comparaMesa = new Comparator<Pedido>() { 
+        @Override
+        public int compare(Pedido  p1, Pedido p2) {
+            return p2.getIdMesa() < p1.getIdMesa() ?-1 :(p2.getIdMesa()==p1.getIdMesa() ?0:1);
+        }
+    };
+    
+    public List<Pedido> listarPedidosPorMesero(String mesero){
+        List<Pedido> pedidosPorMesero = new ArrayList<>();
+        for(Pedido pedido :this.listarPedidos()){
+            if(mesero.equals(pedido.getNombreMesero()))
+                pedidosPorMesero.add(pedido);
+        }
+        Collections.sort(pedidosPorMesero,comparaMesero);
+        return pedidosPorMesero;
+    }    
+    
+    public static Comparator<Pedido> comparaMesero = new Comparator<Pedido>() { 
+        @Override
+        public int compare(Pedido  p1, Pedido p2) {
+            return p1.getNombreMesero().compareTo(p2.getNombreMesero());
+        }
+    };
+    
+    public List<Pedido> listarPedidosPorCobrada(boolean cobrada){
+        List<Pedido> pedidosPorCobrada = new ArrayList<>();
+        for(Pedido pedido :this.listarPedidos()){
+            if(cobrada == pedido.isCobrada())
+                pedidosPorCobrada.add(pedido);
+        }        
+        return pedidosPorCobrada;
+    }    
+    
+    public List<Pedido> listarPedidosPorImporte(double bot, double top){
+        List<Pedido> pedidosPorImporte = new ArrayList<>();
+        for(Pedido pedido :this.listarPedidos()){
+            if(pedido.getImporte()>=bot&&pedido.getImporte()<=top)
+                pedidosPorImporte.add(pedido);
+        }
+        Collections.sort(pedidosPorImporte,comparaImporte);
+        return pedidosPorImporte;
+    }    
+    
+    public static Comparator<Pedido> comparaImporte = new Comparator<Pedido>() { 
+        @Override
+        public int compare(Pedido  p1, Pedido p2) {
+            return p2.getImporte() < p1.getImporte() ?-1 :(p2.getImporte()==p1.getImporte() ?0:1);
+        }
+    };
+    
+    public List<Pedido> listarPedidosPorFecha(LocalDateTime bot, LocalDateTime top){
+        List<Pedido> pedidosPorImporte = new ArrayList<>();
+        for(Pedido pedido :this.listarPedidos()){
+            if(pedido.getFechaHora().isAfter(bot)&&pedido.getFechaHora().isBefore(top))
+                pedidosPorImporte.add(pedido);
+        }
+        Collections.sort(pedidosPorImporte,comparaFechaHora);
+        return pedidosPorImporte;
+    }    
+    
+    public static Comparator<Pedido> comparaFechaHora = new Comparator<Pedido>() { 
+        @Override
+        public int compare(Pedido  p1, Pedido p2) {
+            return p1.getFechaHora().compareTo(p2.getFechaHora());
+        }
+    };
 }
