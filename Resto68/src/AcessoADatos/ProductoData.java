451910b2c5ba52bbfcb3cfadb2882d64d773c161
abril minatta <aminattasosa@gmail.com>
2023-10-11 02:14:26 -0300

    productoData 11/10 02:14

--- Resto68/src/AcessoADatos/ProductoData.java
+++ Resto68/src/AcessoADatos/ProductoData.java
@@ -12,6 +12,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import javax.swing.JOptionPane;
@@ -165,37 +166,7 @@ public class ProductoData {
         return producto;
 
     }
-    public Producto buscarProductoPorPrecio(int precio){
-        Producto producto = null;
-        String sql = "SELECT idProducto, nombre, stock, estado FROM producto WHERE precio = ?";
-        try {
-            PreparedStatement ps = con.prepareStatement(sql);
-            ps.setInt(1, precio);
-            ResultSet rs= ps.executeQuery();
-            
-            if(precio>=0 && precio<=300){
-                
-            }
-            
-            if(rs.next()){
-                producto = new Producto();
-                producto.setPrecio(precio);
-                producto.setIdProducto(rs.getInt("idProducto"));
-                producto.setNombre(rs.getString("nombre"));
-                producto.setStock(rs.getInt("stock"));
-                producto.setPrecio(rs.getInt("precio"));
-                producto.setEstado(rs.getBoolean("estado")); //REVISAR SI SE PUEDE HACER ASÍ
-            }else{
-                JOptionPane.showMessageDialog(null, "No existe este producto en la tabla");
-            }
-            rs.close();
-        } catch (SQLException ex) {
-            //Logger.getLogger(ProductoData1.class.getName()).log(Level.SEVERE, null, ex);
-            JOptionPane.showMessageDialog(null, "No se pudo ingresar a la tabla");
-        } 
-        return null;
-    }
-
+    
     public List<Producto> listarProductos() {
         List<Producto> productoList = new ArrayList<>();
 
@@ -224,10 +195,101 @@ public class ProductoData {
         return productoList;
     }
 
+    
+    public List<Producto> buscarProductosMenorAMayorPrecio(int precio){
+        List<Producto> listaProductos = new ArrayList<>();
+        
+        String sql = "SELECT idProducto, nombre, stock, estado FROM producto ORDER BY precio ASC LIMIT 1";//para ordenar de menor a mayor precio
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, precio);
+            ResultSet rs= ps.executeQuery();
+                        
+            if(rs.next()){
+               Producto producto = new Producto();
+                producto.setPrecio(precio);
+                producto.setIdProducto(rs.getInt("idProducto"));
+                producto.setNombre(rs.getString("nombre"));
+                producto.setStock(rs.getInt("stock"));
+                producto.setPrecio(rs.getInt("precio"));
+                producto.setEstado(rs.getBoolean("estado")); //REVISAR SI SE PUEDE HACER ASÍ
+                listaProductos.add(producto);
+            }else{
+                JOptionPane.showMessageDialog(null, "No existe este producto en la tabla");
+            }
+            rs.close();
+        } catch (SQLException ex) {
+            //Logger.getLogger(ProductoData1.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "No se pudo ingresar a la tabla");
+        } 
+        // Ordena la lista de productos por precio ascendente
+    Collections.sort(listaProductos, (p1, p2) -> Integer.compare(p1.getPrecio(), p2.getPrecio()));
+        return listaProductos;
+    }
+
    public static Comparator<Producto> comparaNombre = new Comparator<Producto>() { //SUBCLASE. Lo que está entre <> es el tipo.
         @Override
         public int compare(Producto p1, Producto p2) {
             return p1.getNombre().compareTo(p2.getNombre()); //compara el nmbre de un producto con el otro.
         }
     };
+   //    public ClaseProducto buscarProductoConPrecioMenorA2000(int precio){//quise hacer yo
+//        //List<ClaseProducto> listarProductosPorPrecio = new ArrayList<>();
+//        String sql = "SELECT idProducto, nombre, stock, estado FROM producto WHERE precio = ?";
+//        
+//        PreparedStatement ps;
+//        try {
+//            ps = con.prepareStatement(sql);
+//            ps.setInt(1, precio);
+//            ResultSet rs= ps.executeQuery();
+//            ClaseProducto producto = new ClaseProducto();
+//            if(rs.next()){
+//                
+//                if(precio>=0 && precio<=2000){
+//                
+//                producto.setPrecio(precio);
+//                producto.setIdProducto(rs.getInt("idProducto"));
+//                producto.setNombre(rs.getString("nombre"));
+//                producto.setStock(rs.getInt("stock"));
+//                producto.setPrecio(rs.getInt("precio"));
+//                producto.setEstado(rs.getBoolean("estado"));
+//                
+//                }
+//            }
+//        } catch (SQLException ex) {
+//            Logger.getLogger(ProductoData1.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        return null;
+            
+        
+    }
+    /*
+    public ClaseProducto buscarProductoConPrecioMenorA2000() {//me lo corrigio chatgpt
+    ClaseProducto producto = null;
+    String sql = "SELECT idProducto, nombre, stock, precio, estado FROM producto WHERE precio <= 2000 ORDER BY precio ASC LIMIT 1";
+
+    try {
+        PreparedStatement ps = con.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+
+        if (rs.next()) {
+            producto = new ClaseProducto();
+            producto.setIdProducto(rs.getInt("idProducto"));
+            producto.setNombre(rs.getString("nombre"));
+            producto.setStock(rs.getInt("stock"));
+            producto.setPrecio(rs.getInt("precio"));
+            producto.setEstado(rs.getBoolean("estado"));
+        } else {
+            // Puedes manejar el caso en el que no se encuentren productos con precio menor a 2000 aquí.
+        }
+
+        rs.close();
+    } catch (SQLException ex) {
+        // Maneja la SQLException según tus necesidades.
+        Logger.getLogger(ProductoData1.class.getName()).log(Level.SEVERE, null, ex);
+    }
+
+    return producto;
 }
+    */
+
