d78c154f27906697b8b7e8b91eda26af909d0ed4
Tobias Amaro Miranda <ama44490@gmail.com>
2023-10-24 12:25:49 -0300

    Update MesaData 24/10

--- Resto68/src/AcessoADatos/MesaData.java
+++ Resto68/src/AcessoADatos/MesaData.java
@@ -11,6 +11,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -63,29 +67,6 @@ public class MesaData {
         }
 
     }
-    
-    public Mesa BuscarMesa(int idMesa){
-    Mesa mesa = null;
-    String sql = "SELECT numero, capacidad, estado FROM mesa WHERE idMesa = ?";
-    PreparedStatement ps = null;
-    try { 
-    ps = con.prepareStatement(sql);
-    ps.setInt(1, idMesa);
-    ResultSet rs = ps.executeQuery();
-    if (rs.next()) {
-    mesa = new Mesa();
-    mesa.setIdMesa(idMesa);
-    mesa.setCapacidad(rs.getInt("Capacidad"));
-    mesa.setNumero(rs.getInt("Numero"));
-    mesa.setEstadoDeMesa(rs.getBoolean("Estado"));
-    } else {
-        JOptionPane.showMessageDialog(null, "No existe tal mesa");
-        ps.close();} 
-    }   catch (SQLException ex) { 
-            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla" + ex.getMessage());
-        } 
-    return mesa;
-    }
 
   public void modificarMesa (Mesa mesa){
       String sql = "UPDATE mesa SET numero=? , capacidad=?, estado=? WHERE idMesa = ?";
@@ -102,17 +83,136 @@ public class MesaData {
          }
       } catch (SQLException ex){
       JOptionPane.showMessageDialog(null, "No se pudo modificar la mesa" + ex.getMessage());
-      }
-      
+      } 
   }
+  
+  public List <Mesa> listarMesa(){
+  List<Mesa> ListaDeMesas = new ArrayList<>();
+  String sql = "SELECT * FROM mesa";
+  
+  try {
+     PreparedStatement ps = con.prepareStatement(sql);
+     ResultSet rs = ps.executeQuery();
+     while(rs.next()) {
+         Mesa mesa = new Mesa();
+         mesa.setIdMesa(rs.getInt("idMesa"));
+         mesa.setNumero(rs.getInt("numero"));
+         mesa.setCapacidad(rs.getInt("capacidad"));
+         mesa.setEstadoDeMesa(rs.getBoolean("estado"));
+         ListaDeMesas.add(mesa);
+     }
+     ps.close();
+    } catch (SQLException ex) {
+    JOptionPane.showMessageDialog(null, "Error al ingresar a la tabla" + ex.getMessage());
+    }
+  return ListaDeMesas;
+  }
+  
+  
+   public Mesa buscarMesaPorId(int idMesa){
+        Mesa mesa = null;
+        String sql = "SELECT  numero, capacidad, estado mesa WHERE idMesa=? ";
+        PreparedStatement ps = null;
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, idMesa);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                mesa = new Mesa();
+                mesa.setIdMesa(idMesa);
+                mesa.setIdMesa(rs.getInt("idMesa"));
+                mesa.setNumero(rs.getInt("numero"));
+                mesa.setCapacidad(rs.getInt("capacidad"));
+                mesa.setEstadoDeMesa(rs.getBoolean("estado"));
+            }else {
+                JOptionPane.showMessageDialog(null, "No existe la mesa buscada");                
+            }
+            ps.close();   
+        } catch (SQLException ex) {
+            Logger.getLogger(PedidoData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return mesa;
+    }
 
+   public Mesa buscarMesaPorCapacidad(int Capacidad){
+     Mesa mesa = null;
+     String sql = "SELECT idMesa, numero, estado mesa WHERE capacidad=? ";
+     PreparedStatement ps = null;
+     try {
+         ps = con.prepareStatement(sql);
+         ps.setInt(4, Capacidad);
+         ResultSet rs = ps.executeQuery();
+         if(rs.next()){
+             mesa = new Mesa();
+             mesa.setCapacidad(Capacidad);
+             mesa.setIdMesa(rs.getInt("idMesa"));
+             mesa.setNumero(rs.getInt("numero"));
+             mesa.setCapacidad(rs.getInt("capacidad"));
+             mesa.setEstadoDeMesa(rs.getBoolean("estado"));
+         }else {
+         JOptionPane.showMessageDialog(null, "No existe la mesa buscada");
+         }
+         ps.close();
+     } catch (SQLException ex){
+         Logger.getLogger(PedidoData.class.getName()).log(Level.SEVERE, null, ex);
+     }
+     return mesa;
+}
 
-
-
-
-
-
-
-
-}     
-     
+      public Mesa buscarMesaPorNumero(int Numero){
+     Mesa mesa = null;
+     String sql = "SELECT idMesa, capacidad, estado mesa WHERE numero=? ";
+     PreparedStatement ps = null;
+     try {
+         ps = con.prepareStatement(sql);
+         ps.setInt(1, Numero);
+         ResultSet rs = ps.executeQuery();
+         if(rs.next()){
+             mesa = new Mesa();
+             mesa.setNumero(Numero);
+             mesa.setIdMesa(rs.getInt("idMesa"));
+             mesa.setNumero(rs.getInt("numero"));
+             mesa.setCapacidad(rs.getInt("capacidad"));
+             mesa.setEstadoDeMesa(rs.getBoolean("estado"));
+         }else {
+         JOptionPane.showMessageDialog(null, "No existe la mesa buscada");
+         }
+         ps.close();
+     } catch (SQLException ex){
+         Logger.getLogger(PedidoData.class.getName()).log(Level.SEVERE, null, ex);
+     }
+     return mesa;
+}
+      
+          public Mesa BuscarMesa(int idMesa){
+           Mesa mesa = null;
+           String sql = "SELECT numero, capacidad, estado FROM mesa WHERE idMesa = ?";
+           PreparedStatement ps = null;
+           try { 
+           ps = con.prepareStatement(sql);
+           ps.setInt(1, idMesa);
+           ResultSet rs = ps.executeQuery();
+           if (rs.next()) {
+           mesa = new Mesa();
+           mesa.setIdMesa(idMesa);
+           mesa.setCapacidad(rs.getInt("Capacidad"));
+           mesa.setNumero(rs.getInt("Numero"));
+           mesa.setEstadoDeMesa(rs.getBoolean("Estado"));
+         } else {
+        JOptionPane.showMessageDialog(null, "No existe tal mesa");
+        ps.close();} 
+    }   catch (SQLException ex) { 
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla" + ex.getMessage());
+        } 
+    return mesa;
+    }
+        public List<Mesa> buscarMesaPorEstado(boolean estado) {
+        List<Mesa> listaDeMesas = new ArrayList<>();
+        for (Mesa mesas : this.listarMesa())
+        {
+            if (mesas.isEstadoDeMesa()== estado) {
+                listaDeMesas.add(mesas);
+            }
+    } return listaDeMesas;
+ }
+}
